956e831f90b7d55c80c93701c812cfe79e3fee00
judepark <psw0616a@naver.com>
2023-01-16 11:15:21 +0900

    with improved time complexity

--- src/main/kotlin/programmers/lv0/DeleteaSpecificChar.kt
+++ src/main/kotlin/programmers/lv0/DeleteaSpecificChar.kt
@@ -4,4 +4,7 @@ class DeleteaSpecificChar {
     fun solution(my_string: String, letter: String): String {
         return my_string.replace(letter,"");
     }
+    fun solution2(my_string: String, letter: String): String {
+        return my_string.filterNot { it == letter[0]}
+    } // Better Time complexity
 } // Resource : https://school.programmers.co.kr/learn/courses/30/lessons/120826
