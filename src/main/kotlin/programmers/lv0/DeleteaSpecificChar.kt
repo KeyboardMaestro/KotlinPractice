ddbc0b6bf0ad0322438eb3749461f9a24c0e8982
judepark <psw0616a@naver.com>
2023-01-16 11:12:24 +0900

    Solved

---
+++ src/main/kotlin/programmers/lv0/DeleteaSpecificChar.kt
@@ -0,0 +1,7 @@
+package programmers.lv0
+
+class DeleteaSpecificChar {
+    fun solution(my_string: String, letter: String): String {
+        return my_string.replace(letter,"");
+    }
+} // Resource : https://school.programmers.co.kr/learn/courses/30/lessons/120826
